8a6b2b7fa7187a9cd85b69a5147061ba5dd085f7
Dustin Jenkins <djenkins.cadc@gmail.com>
2019-04-11 10:37:47 -0700

    Fixes to use Spatial Indexes.

--- src/main/resources/sql/alma_obscore_view.sql
+++ src/main/resources/sql/alma_obscore_view.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE FORCE VIEW ALMA.obscore (
     s_dec,
     s_fov,
     s_region,
+    s_region_icrs,
     s_resolution,
     t_min,
     t_max,
@@ -24,38 +25,39 @@ CREATE OR REPLACE FORCE VIEW ALMA.obscore (
     pol_states,
     facility_name,
     instrument_name
- ) AS SELECT 
-    CASE WHEN energy.channel_num > 128 THEN 'cube' 
-         ELSE 'image' END, 
-    CASE WHEN science.product_type = 'MOUS' THEN 2 
-         WHEN science.product_type = 'GOUS' THEN 3 
-         ELSE null END, 
-    'ALMA',  
+ ) AS SELECT
+    CASE WHEN energy.channel_num > 128 THEN 'cube'
+         ELSE 'image' END,
+    CASE WHEN science.product_type = 'MOUS' THEN 2
+         WHEN science.product_type = 'GOUS' THEN 3
+         ELSE null END,
+    'ALMA',
     aous.asa_ous_id,
-    'ADS/JAO.ALMA#' || asap.code, 
+    'ADS/JAO.ALMA#' || asap.code,
     'http://almascience.org/aq?member_ous_id=' || science.member_ouss_id,
     'text/html',
     apf.stored_size,
-    science.source_name, 
-    science.ra, 
-    science.dec, 
-    science.fov, 
+    science.source_name,
+    science.ra,
+    science.dec,
+    science.fov,
+    TO_GEOMETRIC_OBJECT(science.footprint),
     science.footprint,
-    science.spatial_scale_min, 
+    science.spatial_scale_min,
     ts2mjd(science.start_date),
     ts2mjd(science.end_date),
     science.int_time,
-    science.int_time, 
-    (0.299792458/energy.frequency_min), 
-    (0.299792458/energy.frequency_max),
-    energy.resolving_power_max, 
-    'phot.flux.density;phys.polarization', 
-    energy.pol_product, 
-    'JAO', 
+    science.int_time,
+    (0.299792458 / energy.frequency_min),
+    (0.299792458 / energy.frequency_max),
+    energy.resolving_power_max,
+    'phot.flux.density;phys.polarization',
+    energy.pol_product,
+    'JAO',
     'ALMA'
 FROM ALMA.asa_science science
 INNER JOIN ALMA.asa_energy energy ON energy.asa_dataset_id = science.dataset_id
 INNER JOIN ALMA.asa_project asap ON asap.code = science.project_code
-INNER JOIN ALMA.asa_ous aous ON aous.asa_ous_id = science.asa_ous_id  
+INNER JOIN ALMA.asa_ous aous ON aous.asa_ous_id = science.asa_ous_id
 LEFT OUTER JOIN ALMA.asa_product_files apf ON energy.asa_energy_id = apf.asa_energy_id
 WHERE science.product_type = 'MOUS';
